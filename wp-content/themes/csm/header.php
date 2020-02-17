3c2b0dfc2a6ab6837b92be88a56b8af573804b41
DESKTOP-N9C8HF0\Konstantin <trigger.kst@gmail.com>
2020-02-17 23:51:30 +0600

    3

--- wp-content/themes/csm/header.php
+++ wp-content/themes/csm/header.php
@@ -15,7 +15,7 @@
 
   <link rel="stylesheet" href="/wp-content/themes/csm/assets/css/main.css">
 
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/fancyapps/fancybox@3.5.7/dist/jquery.fancybox.min.css" />
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/fancyapps/fancybox@3.5.7/dist/jquery.fancybox.min.css"/>
 
 </head>
 
@@ -23,32 +23,57 @@
 <body <?php body_class(); ?>>
 
 <!--[if IE]>
-<p class="browserupgrade">Вы используете <strong>старую</strong> версию браузера. Пожалуйста, <a href="https://browsehappy.com/">обновите Ваш браузер</a> для правильного отображения сайта.</p>
+<p class="browserupgrade">Вы используете <strong>старую</strong> версию браузера. Пожалуйста, <a
+  href="https://browsehappy.com/">обновите Ваш браузер</a> для правильного отображения сайта.</p>
 <![endif]-->
 <?php
 wp_body_open();
 ?>
 <div class="body_wrapper">
 
-<header id="site-header" class="header-footer-group" role="banner">
+  <header id="site-header" class="header-footer-group" role="banner">
 
-  <div class="in">
-    <div class="pull-left">
-      <a href="/" class="logo pull-left"></a>
+    <div class="in desktop">
+      <div class="pull-left">
+        <a href="/" class="logo pull-left"></a>
+      </div>
+      <div class="pull-right btn-group">
+        <ul class="social-links">
+          <li><a href="https://api.whatsapp.com/send?phone=79257753467" target="_blank" class="ico-wh"></a></li>
+          <li><a href="viber://chat?number=79257753467" target="_blank" class="ico-vb"></a></li>
+          <li><a href="https://t.me/CSMIG" target="_blank" class="ico-tg"></a></li>
+        </ul>
+        <a href="tel:89257713153" class="ico-tel top-tel">8 925 771 31 53</a>
+        <a data-fancybox data-src="#callmeback" href="javascript:;" class="btn">Заказать звонок</a>
+      </div>
+      <div class="clearfix"></div>
     </div>
-    <div class="pull-right btn-group">
-      <ul class="social-links">
-        <li><a href="https://api.whatsapp.com/send?phone=79257753467" target="_blank" class="ico-wh"></a></li>
-        <li><a href="viber://chat?number=79257753467" target="_blank" class="ico-vb"></a></li>
-        <li><a href="https://t.me/CSMIG" target="_blank" class="ico-tg"></a></li>
-      </ul>
-      <a href="tel:89257713153" class="ico-tel top-tel">8 925 771 31 53</a>
-      <a data-fancybox data-src="#callmeback" href="javascript:;" class="btn">Заказать звонок</a>
-    </div>
-    <div class="clearfix"></div>
-  </div>
 
-</header><!-- #site-header -->
+    <div class="mobile topcontacts">
+      <div class="flex">
+        <div class="flex1-2">
+          <a href="/" class="logo"></a>
+        </div>
+        <div class="flex1-2 right">
+          <a data-fancybox data-src="#callmeback" href="javascript:;" class="btn">Заказать звонок</a>
+        </div>
+      </div>
+      <div class="flex tels">
+        <a href="tel:89257713153" class="ico-tel top-tel">8 925 771 31 53</a>
+        <a href="tel:84953693042" class="ico-tel top-tel">8 495 369 30 42</a>
+      </div>
+      <div class="flex">
+        <ul class="social-links">
+          <li><a href="https://api.whatsapp.com/send?phone=79257753467" target="_blank" class="ico-wh"></a></li>
+          <li><a href="viber://chat?number=79257753467" target="_blank" class="ico-vb"></a></li>
+          <li><a href="https://t.me/CSMIG" target="_blank" class="ico-tg"></a></li>
+        </ul>
+      </div>
+
+
+    </div>
+
+  </header><!-- #site-header -->
 
 
 <?php
